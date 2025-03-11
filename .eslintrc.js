ab58f61dad6ccf5ee0e7b1ae208a4fd0d2caaf5d
peyman-hakemi <mohammad.hakemi.far@gmail.com>
2025-03-11 21:16:32 +0330

    add import solter for readable imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     JSX: true,
   },
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native', // Keep your existing React Native configuration [[1]]
+    'eslint:recommended', // Add recommended ESLint rules [[2]]
+    'plugin:prettier/recommended', // Ensure Prettier integration [[10]]
+  ],
+  plugins: ['simple-import-sort'], // Add the import sorting plugin [[5]]
   rules: {
     'no-unused-vars': 0,
     '@typescript-eslint/no-unused-vars': 0,
@@ -16,5 +21,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     '@typescript-eslint/consistent-type-imports': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'simple-import-sort/imports': 'error', // Enable import sorting [[5]]
+    'simple-import-sort/exports': 'error', // Enable export sorting [[5]]
   },
 };
