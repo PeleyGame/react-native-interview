6e3a2979cce455fb0555290dbe4d61fe74f2edaa
ebhdeveloper <ebh.developer@gmail.com>
2025-03-18 00:03:13 +0330

    update color file

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,4 +1,8 @@
 export const colors = {
+  white: 'var(--White, #FFFFFF)',
   background: 'var(--Background, #ECECEC)',
-  primary: 'var(--Primary , #E2D3FF)',
+  primary: 'var(--Primary, #C8B6ED)',
+  textPrimary: 'var(--TextPrimary, #D4D4D4)',
+  neutral_10: 'var(--Neutral_10 , #18181B)',
+  neutral_17: 'var(--Neutral_17 , #28282F)',
 };
