2aa2642413e159dee3982f59c739f9691c93a948
ebh.developer@gmail.com <ebh.developer@gmail.com>
2025-03-17 10:21:18 +0330

    Implemented HomeScreen with Tailwind-based layout and primary background

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+//import liraries
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-primary">
+      <Text>Start of design</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
