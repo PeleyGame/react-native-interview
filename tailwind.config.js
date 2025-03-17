275beeae12507cd40add8484355f1b8777462b9c
ebhdeveloper <ebh.developer@gmail.com>
2025-03-18 00:02:58 +0330

    add design details in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,43 @@ module.exports = {
     extend: {
       colors,
     },
+    fontFamily: {
+      sans: ['Ubuntu', 'sans-serif'],
+    },
+    fontWeight: {
+      bold: 700,
+      medium: 500,
+    },
+    fontSize: {
+      base: [
+        '14px',
+        {
+          lineHeight: '16px',
+          letterSpacing: '0%',
+          verticalAlign: 'middle',
+        },
+      ],
+      medium: [
+        '14px',
+        {
+          lineHeight: '20px',
+          letterSpacing: '0%',
+          verticalAlign: 'middle',
+        },
+      ],
+    },
+    boxShadow: {
+      custom: '0px 0px 0px 0px #00000040',
+    },
+    borderColor: {
+      custom: colors.primary,
+      textPrimary: colors.textPrimary,
+    },
+    borderWidth: {
+      custom: '2px',
+      textPrimary: '1px',
+    },
   },
   plugins: [],
-  // Add the NativeWind preset
   presets: [require('nativewind/preset')],
 };
